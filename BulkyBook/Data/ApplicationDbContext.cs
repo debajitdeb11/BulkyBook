ede451809272dc9441b3294245e0bedfa231570d
Debajit Deb <debajit.deb@onsolve.com>
2022-12-28 04:41:36 +0530

    Initial Commit

---
+++ BulkyBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Data
+{
+	public class ApplicationDbContext :DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
