712f8952d5235119a91d5fdb0fb982e53f8c5137
Debajit Deb <debajit.deb@onsolve.com>
2022-12-28 15:29:25 +0530

    Connection to Database done

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BulkyBook.Data;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,47 @@ namespace BulkyBook.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+            IEnumerable<Category> categoryList = _db.Categories;
+
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Categories.Add(category);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                } else
+                {
+                    return View(category);
+                }
+            } catch (Exception e)
+            {
+                // TODO: Remaining
+                return RedirectToAction("Index");
+            }
+            
         }
     }
 }
